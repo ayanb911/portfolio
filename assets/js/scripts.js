6979fc37d77f8030c744807b3f7eca7e60050f30
ayanb911 <ayanb911@gmail.com>
2017-07-13 15:43:40 +0530

    header done, first section completed

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        fixedHeader();
+    })
+})
+
+function fixedHeader(){
+    var scroll = $(window).scrollTop(),
+        menu = $("#menu");
+    console.log(scroll);
+    if(scroll >= 100){
+        menu.addClass("center-shadow");
+    }
+    else{
+        menu.removeClass("center-shadow");
+    }
+}
