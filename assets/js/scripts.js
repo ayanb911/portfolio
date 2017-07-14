559fbd3d24c519859a8053f7abe7df111eb270a6
ayanb911 <ayanb911@gmail.com>
2017-07-14 14:48:58 +0530

    projects sections js done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 function fixedHeader(){
     var scroll = $(window).scrollTop(),
         menu = $("#menu");
-    console.log(scroll);
+    //console.log(scroll);
     if(scroll >= 100){
         menu.addClass("center-shadow");
     }
@@ -23,12 +23,27 @@ function projectScroll(){
     projectList.each(function (i) {
         var that = $(this);
         setTimeout(function () {
-            console.log(i);
+            var id = "#"+that.attr("data-info");
+            var notId = $(".project-pop").not(id);
             that.addClass("is-active");
+            $(id).removeClass("hidden");
+            $(id).find(".pictures").addClass("is-active");
+            setTimeout(function () {
+                $(id).find(".pictures img").addClass("is-animating");
+                $(id).find(".content").addClass("is-active");
+            }, 300);
+
             $(".project").not(that).removeClass("is-active");
-        }, i*2000);
+            notId.addClass("hidden").removeClass("is-active");
+            notId.find(".pictures").removeClass("is-active");
+            setTimeout(function () {
+                notId.find(".pictures img").removeClass("is-animating");
+                notId.find(".content").removeClass("is-active");
+            }, 300);
+        }, i*4000);
     });
     setTimeout(function(){
         projectScroll();
-    }, 2000* projectList.length - 1);
+    }, 4000* projectList.length - 1);
 }
+
