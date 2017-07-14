50c92d00c48830e7b8f7be48fc45310df077aafb
ayanb911 <ayanb911@gmail.com>
2017-07-14 12:01:23 +0530

    project section modified, images added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    fixedHeader();
+    projectScroll();
     $(window).scroll(function () {
         fixedHeader();
     })
@@ -14,4 +16,19 @@ function fixedHeader(){
     else{
         menu.removeClass("center-shadow");
     }
+}
+
+function projectScroll(){
+    var projectList = $(".project");
+    projectList.each(function (i) {
+        var that = $(this);
+        setTimeout(function () {
+            console.log(i);
+            that.addClass("is-active");
+            $(".project").not(that).removeClass("is-active");
+        }, i*2000);
+    });
+    setTimeout(function(){
+        projectScroll();
+    }, 2000* projectList.length - 1);
 }
