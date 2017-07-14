17d5b933c3de6ac02a7acd6224e45653315f00cc
ayanb911 <ayanb911@gmail.com>
2017-07-14 16:40:20 +0530

    Project hover done, social images added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     fixedHeader();
-    projectScroll();
+    projectListScroll();
     $(window).scroll(function () {
         fixedHeader();
     })
 })
+
 
 function fixedHeader(){
     var scroll = $(window).scrollTop(),
@@ -18,11 +19,49 @@ function fixedHeader(){
     }
 }
 
-function projectScroll(){
+function projectListScroll() {
     var projectList = $(".project");
+    projectList.hover(
+        function () {
+            var id = "#" + $(this).attr("data-info");
+            var notId = $(".project-pop").not(id);
+
+            $(".project").not(this).removeClass("is-active");
+            notId.addClass("hidden").removeClass("is-active");
+            notId.find(".pictures").removeClass("is-active");
+            setTimeout(function () {
+                notId.find(".pictures img").removeClass("is-animating");
+                notId.find(".content").removeClass("is-active");
+            }, 300);
+
+            $(this).addClass("is-active");
+            $(id).removeClass("hidden");
+            $(id).find(".pictures").addClass("is-active");
+            setTimeout(function () {
+                $(id).find(".pictures img").addClass("is-animating");
+                $(id).find(".content").addClass("is-active");
+            }, 300);
+        }
+    )
+}
+
+
+
+/*function projectScroll(){
+    var id = "#" + $(this).attr("data-info");
+    var notId = $(".project-pop").not(id);
+
+    var projectList = $(".project");
+    $(".project").not(this).removeClass("is-active");
+    notId.addClass("hidden").removeClass("is-active");
+    notId.find(".pictures").removeClass("is-active");
+    setTimeout(function () {
+        notId.find(".pictures img").removeClass("is-animating");
+        notId.find(".content").removeClass("is-active");
+    }, 300);
     projectList.each(function (i) {
         var that = $(this);
-        setTimeout(function () {
+        hoveringProject = setTimeout(function () {
             var id = "#"+that.attr("data-info");
             var notId = $(".project-pop").not(id);
             that.addClass("is-active");
@@ -41,9 +80,14 @@ function projectScroll(){
                 notId.find(".content").removeClass("is-active");
             }, 300);
         }, i*4000);
+        projectList.hover(function () {
+            return;
+        })
     });
+
     setTimeout(function(){
         projectScroll();
     }, 4000* projectList.length - 1);
-}
+
+}*/
 
