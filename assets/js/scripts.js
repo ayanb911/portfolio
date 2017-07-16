f3d8984a3c723981487e71f0442e6b8d7f1a4467
ayan <ayan7414@gmail.com>
2017-07-16 12:04:39 +0530

    footer design changed, icons added, row-icons structure changed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
     projectListScroll();
-    siteMade();
 })
 
 
 /*function fixedHeader(){
-    var scroll = $(window).scrollTop(),
-        menu = $("#menu");
-    //console.log(scroll);
-    if(scroll >= 100){
-        menu.addClass("center-shadow");
-    }
-    else{
-        menu.removeClass("center-shadow");
-    }
-}*/
+ var scroll = $(window).scrollTop(),
+ menu = $("#menu");
+ //console.log(scroll);
+ if(scroll >= 100){
+ menu.addClass("center-shadow");
+ }
+ else{
+ menu.removeClass("center-shadow");
+ }
+ }*/
 
 function projectListScroll() {
     var projectList = $(".project");
@@ -42,69 +41,48 @@ function projectListScroll() {
     )
 }
 
-function siteMade(){
-    var hoverHtml = '<div class="row-icons">'+
-'<img src="assets/images/coding%20icons/html5.png" title="Photoshop">'+
-        '<img src="assets/images/coding%20icons/sass.svg" title="PHP" class="php">'+
-        '<img src="assets/images/coding%20icons/jquery.gif" title="Drupal" class="drupal">'+
-        '<img src="assets/images/coding%20icons/git.png" title="Drupal" class="drupal">'+
-        '<img src="assets/images/coding%20icons/gulp.png" title="Drupal" class="drupal">'+
-        '</div>';
-    var link = $("#contact>.info");
-
-    console.log(hoverHtml);
-
-    link.hover(function () {
-        link.html(hoverHtml);
-        console.log(1);
-    },
-    function () {
-        link.html('<p>How was this site made?</p>');
-        console.log(2);
-    })
-}
 
 /*function projectScroll(){
-    var id = "#" + $(this).attr("data-info");
-    var notId = $(".project-pop").not(id);
+ var id = "#" + $(this).attr("data-info");
+ var notId = $(".project-pop").not(id);
 
-    var projectList = $(".project");
-    $(".project").not(this).removeClass("is-active");
-    notId.addClass("hidden").removeClass("is-active");
-    notId.find(".pictures").removeClass("is-active");
-    setTimeout(function () {
-        notId.find(".pictures img").removeClass("is-animating");
-        notId.find(".content").removeClass("is-active");
-    }, 300);
-    projectList.each(function (i) {
-        var that = $(this);
-        hoveringProject = setTimeout(function () {
-            var id = "#"+that.attr("data-info");
-            var notId = $(".project-pop").not(id);
-            that.addClass("is-active");
-            $(id).removeClass("hidden");
-            $(id).find(".pictures").addClass("is-active");
-            setTimeout(function () {
-                $(id).find(".pictures img").addClass("is-animating");
-                $(id).find(".content").addClass("is-active");
-            }, 300);
+ var projectList = $(".project");
+ $(".project").not(this).removeClass("is-active");
+ notId.addClass("hidden").removeClass("is-active");
+ notId.find(".pictures").removeClass("is-active");
+ setTimeout(function () {
+ notId.find(".pictures img").removeClass("is-animating");
+ notId.find(".content").removeClass("is-active");
+ }, 300);
+ projectList.each(function (i) {
+ var that = $(this);
+ hoveringProject = setTimeout(function () {
+ var id = "#"+that.attr("data-info");
+ var notId = $(".project-pop").not(id);
+ that.addClass("is-active");
+ $(id).removeClass("hidden");
+ $(id).find(".pictures").addClass("is-active");
+ setTimeout(function () {
+ $(id).find(".pictures img").addClass("is-animating");
+ $(id).find(".content").addClass("is-active");
+ }, 300);
 
-            $(".project").not(that).removeClass("is-active");
-            notId.addClass("hidden").removeClass("is-active");
-            notId.find(".pictures").removeClass("is-active");
-            setTimeout(function () {
-                notId.find(".pictures img").removeClass("is-animating");
-                notId.find(".content").removeClass("is-active");
-            }, 300);
-        }, i*4000);
-        projectList.hover(function () {
-            return;
-        })
-    });
+ $(".project").not(that).removeClass("is-active");
+ notId.addClass("hidden").removeClass("is-active");
+ notId.find(".pictures").removeClass("is-active");
+ setTimeout(function () {
+ notId.find(".pictures img").removeClass("is-animating");
+ notId.find(".content").removeClass("is-active");
+ }, 300);
+ }, i*4000);
+ projectList.hover(function () {
+ return;
+ })
+ });
 
-    setTimeout(function(){
-        projectScroll();
-    }, 4000* projectList.length - 1);
+ setTimeout(function(){
+ projectScroll();
+ }, 4000* projectList.length - 1);
 
-}*/
+ }*/
 
